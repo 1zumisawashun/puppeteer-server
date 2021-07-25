20d9b21baa9bff0e762c61a76db12fc3ba485bb8
izumisawashun <shun.1zumisawa@gmail.com>
2021-07-25 18:49:10 +0900

    add-json-web-token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,16 @@
+const firebase = require("../plugins/firebase");
+const db = firebase.firestore();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//create token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "net ninja secret", {
+    expiresIn: maxAge,
+  });
+};
+
 const signup_get = (req, res) => {
   res.render("signup", { title: "signup" });
 };
@@ -5,10 +18,38 @@ const login_get = (req, res) => {
   res.render("login", { title: "login" });
 };
 
-const login_post = (req, res) => {
-  const { email, password } = res.body;
-  console.log(email, password);
-  res.send("user login", { title: "user login" });
+const login_post = async (req, res) => {
+  const { email, password } = req.body;
+  await db
+    .collection("users")
+    .where("email", "==", email)
+    .get()
+    .then((docSnapshot) => {
+      const user = [];
+      docSnapshot.forEach((doc) => {
+        user.push({ id: doc.id, ...doc.data() });
+      });
+      return user[0];
+    })
+    .then(async (user) => {
+      console.log(user);
+      const newErrors = {};
+      if (user) {
+        const auth = await bcrypt.compare(password, user.hashPassword);
+        if (auth) {
+          console.log(auth, "authはあります！");
+          const token = await createToken(user.id);
+          res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1800 });
+          return res.status(201).json({ user: user.id });
+          //pass with data to login.ejs
+        }
+        newErrors.password = "inncorect password!";
+        return res.status(422).json(newErrors);
+        //already linked to login.ejs
+      }
+      newErrors.email = "inncorect email!";
+      return res.status(422).json(newErrors);
+    });
 };
 module.exports = {
   signup_get,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,31 +5,57 @@ const { check, validationResult } = require("express-validator");
 const firebase = require("../plugins/firebase");
 const db = firebase.firestore();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//create token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "net ninja secret", {
+    expiresIn: maxAge,
+  });
+};
 
 router.get("/login", authController.login_get);
 router.post("/login", authController.login_post);
 router.get("/signup", authController.signup_get);
 router.post(
   "/signup",
-  [check("email").isEmail(), check("password").isLength({ min: 5 })],
+  [check("email").isEmail(), check("password").isLength({ min: 6 })],
+  //we need to add validation of same email
   async (req, res) => {
     const { email, password } = req.body;
     //validation here
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      // validations failed!
-      return res.status(422).json({ errors: errors.array() });
+      // failed!
+      const newErrors = {};
+      errors.errors.forEach((err, index) => {
+        if (err.param.includes("email")) {
+          newErrors.email = "Please enter a valid email";
+        } else if (err.param.includes("password")) {
+          newErrors.password = "Minimum password length is a 6 characters";
+        }
+      });
+      return res.status(422).json(newErrors);
+      //this data send to signup.ejs and possible to get res.json()
     }
     //bcript here
     const salt = await bcrypt.genSalt();
     const hashPassword = await bcrypt.hash(password, salt);
     //firestore here
-    const user = await db.collection("users").add({
-      email,
-      hashPassword,
-    });
-    res.status(201).json(user);
-    //res.send("new signup", { title: "new signup" });
+    const user = await db
+      .collection("users")
+      .add({
+        email,
+        hashPassword,
+      })
+      .then(async (result) => {
+        const token = await createToken(result.id);
+        res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1800 });
+        res.status(201).json({ user: result.id });
+        //this data send to signup.ejs and possible to get res.json()
+      });
   }
 );
+
 module.exports = router;
