a69315d3e4428ee748de5988d1c9e69b2049c7ad
1zumisawashun <shun.1zumisawa@gmail.com>
2022-03-05 11:45:40 +0900

    スクレイピングのリファクタ

--- middleware/scrapingMiddleware.js
+++ middleware/scrapingMiddleware.js
@@ -5,8 +5,8 @@ const executablePath =
 const scraping = async (shop) => {
   const browser = await puppeteer.launch({
     executablePath: executablePath,
-    slowMo: 500,
-    headless: false,
+    // slowMo: 50,
+    headless: true,
   });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
@@ -14,26 +14,46 @@ const scraping = async (shop) => {
     waitUntil: "networkidle2",
   });
 
-  // scraping
-  const priceSelector = await page.$$(shop.pricePath);
-  const nameSelector = await page.$$(shop.namePath);
-  const resultArray = [];
+  // NOTE:商品価格のスクレイピング
+  const priceDatas = await page.evaluate((selector) => {
+    const list = Array.from(document.querySelectorAll(selector));
+    return list.map((data, index) => {
+      return {
+        id: index,
+        price: data.textContent,
+      };
+    });
+  }, shop.pricePath);
 
-  for (let i = 0; i < priceSelector.length; i++) {
-    const data = {
-      price: await (
-        await priceSelector[i].getProperty("textContent")
-      ).jsonValue(),
-      name: await (
-        await nameSelector[i].getProperty("textContent")
-      ).jsonValue(),
-    };
-    resultArray.push(data);
-  }
+  // NOTE:商品名のスクレイピング
+  const nameDatas = await page.evaluate((selector) => {
+    const list = Array.from(document.querySelectorAll(selector));
+    return list.map((data, index) => {
+      return {
+        id: index,
+        name: data.textContent,
+      };
+    });
+  }, shop.namePath);
+
+  // NOTE:プロパティアクセスできるように加工する
+  const formatNameDatas = nameDatas.reduce(
+    (accumulator, currentValue, index) => {
+      accumulator[index] = currentValue;
+      return accumulator;
+    },
+    {}
+  );
+
+  // NOTE:一致するオブジェクトをマージさせる
+  const result = priceDatas.map((el) => {
+    return formatNameDatas[el.id]
+      ? { ...formatNameDatas[el.id], price: el.price }
+      : "データが取得できませんでした";
+  });
 
   await browser.close();
-  console.log(resultArray, "check resultArray");
-  return resultArray;
+  return result;
 };
 
 module.exports = { scraping };
